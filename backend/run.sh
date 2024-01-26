697b25b7d88a86b23ff396272b253993d87260c6
devsunset <devsunset@gmail.com>
2024-01-26 16:19:20 +0900

    dev

--- backend/run.sh
+++ backend/run.sh
@@ -2,14 +2,14 @@
 
 export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
-export PORT=${PORT:-80}
-# export PORT=${PORT:-8181}
+# export PORT=${PORT:-80}
+export PORT=${PORT:-8181}
 
 # http
-exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
+# exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE"
 
-# https sunsetserver.mooo.com SSL for Free 
-#exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE" --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
+#https sunsetserver.mooo.com SSL for Free 
+exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE" --ssl-keyfile=./private.key --ssl-certfile=./certificate.crt
 
 # https local cert
 #exec uvicorn --reload --host $HOST --port $PORT "$APP_MODULE" --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
